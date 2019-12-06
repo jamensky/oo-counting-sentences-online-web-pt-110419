639d45e33140c6e5f4b397a9c3664434590cc2fc
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-06 22:12:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,24 @@ require 'pry'
 
 class String
 
+  def self
+     self
+  end
+
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    return self.scan(/[.!?;]/).count
   end
+
 end
